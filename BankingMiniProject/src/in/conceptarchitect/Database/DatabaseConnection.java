89aac198a6ede962e2cc27e435514e2d2ce5ffb4
Kottresha <83581464+Kottresha@users.noreply.github.com>
2021-11-07 07:46:48 +0530

    Update DatabaseConnection.java

--- BankingMiniProject/src/in/conceptarchitect/Database/DatabaseConnection.java
+++ BankingMiniProject/src/in/conceptarchitect/Database/DatabaseConnection.java
@@ -14,36 +14,32 @@ import in.conceptarchitect.finance.Transactions;
 
 public class DatabaseConnection {
 
-public List<Transactions> getAllTransactions(int accountnumber) throws Exception {
-List<Transactions> li=new ArrayList<Transactions>();
+	public List<Transactions> getAllTransactions(int accountnumber) throws Exception {
+		List<Transactions> li=new ArrayList<Transactions>();
 	
-	Connection c=DriverManager.getConnection("jdbc:mysql://localhost:3306/project","Banking","Banking");
+		Connection c=DriverManager.getConnection("jdbc:mysql://localhost:3306/project","Banking","Banking");
 
-	PreparedStatement ps=c.prepareStatement("select * from transactions where accountNumber="+accountnumber+";");
+		PreparedStatement ps=c.prepareStatement("select * from transactions where accountNumber="+accountnumber+";");
 	
-	ResultSet rs=ps.executeQuery();
+		ResultSet rs=ps.executeQuery();
 	
-	while(rs.next()) {
-		li.add(new Transactions(rs.getString(1),rs.getString(2),rs.getInt(3),rs.getDouble(4),rs.getDate(5)));
-	}
+		while(rs.next()) {
+			li.add(new Transactions(rs.getString(1),rs.getString(2),rs.getInt(3),rs.getDouble(4),rs.getDate(5)));
+		}
 				
-	c.close();
+		c.close();
 
-	return li;
-}
+		return li;
+	}
 
-public static void insertQuery(String sqlQuery) throws SQLException {
+	public static void insertQuery(String sqlQuery) throws SQLException {
 	// TODO Auto-generated method stub
-	Connection c=DriverManager.getConnection("jdbc:mysql://localhost:3306/project","Banking","Banking");
+		Connection c=DriverManager.getConnection("jdbc:mysql://localhost:3306/project","Banking","Banking");
 	
-	Statement stmt=c.createStatement();
+		Statement stmt=c.createStatement();
 	
-	stmt.executeUpdate(sqlQuery);
-	
-	
-}
-
-
+		stmt.executeUpdate(sqlQuery);
+	}
 	
 	public boolean insertTransactions(Transactions t,AccountData ad)throws Exception{
 		
@@ -58,73 +54,4 @@ public static void insertQuery(String sqlQuery) throws SQLException {
 		
 		return b;
 	}
-	
-	public boolean insertBank(BankDB db)throws Exception{
-		
-		Connection c=DriverManager.getConnection("jdbc:mysql://localhost:3306/Banking","root","root");
-		
-		Statement stmt=c.createStatement();
-		
-		
-		boolean b=stmt.execute("insert into Bank values('"+db.getBankName()+"',"+db.getBankId()+","+db.getBankId()+","+db.getBankId()+")");
-		
-		c.close();
-		
-		return b;
-	}
-
-	public boolean insertSavings(AccountData ad)throws Exception{
-		
-		Connection c=DriverManager.getConnection("jdbc:mysql://localhost:3306/Banking","root","root");
-		
-		Statement stmt=c.createStatement();
-		
-		boolean b=stmt.execute("insert into Savings values("+ad.getAccountNumber()+",'"+ad.getHolderName()+"','"+ad.getAccountType()+"','"+ad.getPassword()+"',"+ad.getBalance()+",default)");
-		
-		c.close();
-		
-		return b;
-	}
-
-	public boolean insertCurrents(AccountData ad)throws Exception{
-		
-		Connection c=DriverManager.getConnection("jdbc:mysql://localhost:3306/Banking","root","root");
-		
-		Statement stmt=c.createStatement();
-		
-		boolean b=stmt.execute("insert into Currents values("+ad.getAccountNumber()+",'"+ad.getHolderName()+"','"+ad.getAccountType()+"','"+ad.getPassword()+"',"+ad.getBalance()+",default)");
-		
-		c.close();
-		
-		return b;
-	}
-	public boolean insertODT(AccountData ad)throws Exception{
-		
-		Connection c=DriverManager.getConnection("jdbc:mysql://localhost:3306/Banking","root","root");
-		
-		Statement stmt=c.createStatement();
-		
-		boolean b=stmt.execute("insert into ODT values("+ad.getAccountNumber()+",'"+ad.getHolderName()+"','"+ad.getAccountType()+"','"+ad.getPassword()+"',"+ad.getBalance()+",default)");
-		
-		c.close();
-		
-		return b;
-	}
-	
-
-	public boolean insertAccount(MainAccountData madb)throws Exception{
-
-		
-		Connection c=DriverManager.getConnection("jdbc:mysql://localhost:3306/Banking","root","root");
-		
-		Statement stmt=c.createStatement();
-		
-		boolean b=stmt.execute("insert into Accounts values("+madb.getAccountNumber()+",'"+madb.getAccountType()+"',"+madb.getBankid()+")");
-		
-		c.close();
-		
-		return b;
-	}
-
-
 }
